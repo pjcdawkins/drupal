8315c56d8251cefb2a640d8cf9f48820a6cf32ca
Klaus Purer <klaus.purer@gmail.com>
2014-01-13 00:10:14 +0100

    Fixed the case of NULL field items in term parent validator.

--- core/modules/taxonomy/lib/Drupal/taxonomy/Plugin/Validation/Constraint/TermParentConstraintValidator.php
+++ core/modules/taxonomy/lib/Drupal/taxonomy/Plugin/Validation/Constraint/TermParentConstraintValidator.php
@@ -19,11 +19,13 @@ class TermParentConstraintValidator extends ConstraintValidator {
    * {@inheritdoc}
    */
   public function validate($field_item, Constraint $constraint) {
-    $parent_term_id = $field_item->value;
-    // If a non-0 parent term id is specified, ensure it corresponds to a real
-    // term in the same vocabulary.
-    if ($parent_term_id && !\Drupal::entityManager()->getStorageController('taxonomy_term')->loadByProperties(array('tid' => $parent_term_id, 'vid' => $field_item->getEntity()->vid->value))) {
-      $this->context->addViolation($constraint->message, array('%id' => $parent_term_id));
+    if ($field_item) {
+      $parent_term_id = $field_item->value;
+      // If a non-0 parent term id is specified, ensure it corresponds to a real
+      // term in the same vocabulary.
+      if ($parent_term_id && !\Drupal::entityManager()->getStorageController('taxonomy_term')->loadByProperties(array('tid' => $parent_term_id, 'vid' => $field_item->getEntity()->vid->value))) {
+        $this->context->addViolation($constraint->message, array('%id' => $parent_term_id));
+      }
     }
   }
 }
